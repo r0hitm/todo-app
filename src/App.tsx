2f07003afb2b111f122882e02aa18b731d06b639
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-20 16:05:24 +0530

    WIP: UI is crap but hooks are working. ISSUe: double rendering

--- src/App.tsx
+++ src/App.tsx
@@ -51,11 +51,19 @@ function App() {
                     </ul>
                 </nav>
                 <div className="add-list">
-                    {/* <NavLink to="/list/new">Add List</NavLink> */}
                     <h3>Add List</h3>
                     <form
                         className="add-list-form"
-                        // TODO
+                        onSubmit={e => {
+                            e.preventDefault();
+                            const form = e.target as HTMLFormElement;
+                            const formData = new FormData(form);
+                            const listName = formData.get(
+                                "list-name"
+                            ) as string;
+                            addList(listName);
+                            form.reset();
+                        }}
                     >
                         <input
                             type="text"
@@ -93,17 +101,88 @@ function App() {
                     <h1>{data?.currentList.name}</h1>
                     {/* <span>Edit</span> */}
                 </header>
-                <div className="attributions">
-                    <a
-                        target="_blank"
-                        href="https://icons8.com/icon/tIUSbVurTTrH/todo-list"
-                    >
-                        Todo
-                    </a>{" "}
-                    icon by{" "}
-                    <a target="_blank" href="https://icons8.com">
-                        Icons8
-                    </a>
+                <div className="tasks">
+                    <div className="list-items">
+                        {data?.currentList.todos.map(todo_item => (
+                            <div className="list-item" key={todo_item.id}>
+                                <div className="checkbox">
+                                    <input
+                                        type="checkbox"
+                                        checked={todo_item.complete}
+                                        onChange={() => {
+                                            toggleTodoItem(todo_item.id);
+                                        }}
+                                    />
+                                </div>
+                                <div className="todo-item">
+                                    <div className="todo-item-name">
+                                        {todo_item.title}
+                                    </div>
+                                    <div className="todo-item-description">
+                                        {todo_item.description}
+                                    </div>
+                                    <div className="todo-item-due-date">
+                                        {todo_item.dueDate?.toDateString()}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="add-todo-item">
+                        <h3>Add Todo Item</h3>
+                        <form
+                            className="add-todo-item-form"
+                            onSubmit={e => {
+                                e.preventDefault();
+                                const form = e.target as HTMLFormElement;
+                                const formData = new FormData(form);
+                                const title = formData.get(
+                                    "todo-item-title"
+                                ) as string;
+                                const description = formData.get(
+                                    "todo-item-description"
+                                ) as string;
+                                const dueDate = formData.get(
+                                    "todo-item-due-date"
+                                ) as string;
+                                addTodoItem(
+                                    title,
+                                    description,
+                                    new Date(dueDate)
+                                );
+                                form.reset();
+                            }}
+                        >
+                            <input
+                                type="text"
+                                name="todo-item-title"
+                                placeholder="Enter Todo Item Title"
+                            />
+                            <input
+                                type="text"
+                                name="todo-item-description"
+                                placeholder="Enter Todo Item Description"
+                            />
+                            <input
+                                type="date"
+                                name="todo-item-due-date"
+                                placeholder="Enter Todo Item Due Date"
+                            />
+                            <button type="submit">Add</button>
+                        </form>
+                    </div>
+                    <div className="attributions">
+                        <a
+                            target="_blank"
+                            href="https://icons8.com/icon/tIUSbVurTTrH/todo-list"
+                        >
+                            Todo
+                        </a>{" "}
+                        icon by{" "}
+                        <a target="_blank" href="https://icons8.com">
+                            Icons8
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
