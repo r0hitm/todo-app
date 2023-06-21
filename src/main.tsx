b9a901df864f40ae9bc9ce956c963e4ca49b82d6
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-21 12:49:15 +0530

    wip: setting up react router to navigate b/w sign-up, sign-in and logout routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
-// import Root from "./routes/root.tsx";
-// import { ErrorPage } from "./routes/error.tsx";
 
-// import { root_loader, list_loader } from "./loaders.ts";
-
-// const router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <Root />,
-//         errorElement: <ErrorPage />,
-//         loader: root_loader,
-//         children: [
-//             {
-//                 path: "/list/:id",
-//                 element: <Root />,
-//                 errorElement: <ErrorPage />,
-//                 loader: list_loader
-//             }
-//         ],
-//     },
-// ]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <App />,
+        },
+    ],
+    { basename: "/todo-app" }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        {/* <RouterProvider router={router} /> */}
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
