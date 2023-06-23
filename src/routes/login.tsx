c762ee08deb038296320190ef4a28a5f1e419a95
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-23 14:32:46 +0530

    user can sign up and log in and use the todo

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,42 +1,22 @@
 /**
  * Login route that handles the login process using firebase auth
  */
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
 
 export default function Login() {
     return (
         <div className="login-form-wrapper">
-            <form
-                className="login-form"
-                onSubmit={async e => {
-                    e.preventDefault();
-                    const form = e.target as HTMLFormElement;
-                    const email = form.email as HTMLInputElement;
-                    const password = form.password as HTMLInputElement;
-                    try {
-                        await signInWithEmailAndPassword(
-                            auth,
-                            email.value,
-                            password.value
-                        );
-                    } catch (error) {
-                        alert(error);
-                    }
-                }}
-            >
+            <Form className="login-form" method="POST">
                 <h1>Login</h1>
                 <p>
-                    Don't have an account?{" "}
-                    <Link to="/signup">Sign up</Link>
+                    Don't have an account? <Link to="/signup">Sign up</Link>
                 </p>
                 <label htmlFor="email">Email</label>
                 <input type="email" name="email" id="email" required />
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" id="password" required />
                 <button type="submit">Login</button>
-            </form>
+            </Form>
         </div>
     );
 }
--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,36 +1,12 @@
 /**
  * Signup route that handles the signup process using firebase auth for the new user
  */
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
 
 export default function Signup() {
     return (
         <div className="login-form-wrapper">
-            <form
-                className="login-form"
-                onSubmit={async e => {
-                    e.preventDefault();
-                    const form = e.target as HTMLFormElement;
-                    const email = form.email as HTMLInputElement;
-                    const password = form.password as HTMLInputElement;
-                    const confirm = form.confirm as HTMLInputElement;
-                    if (password.value !== confirm.value) {
-                        alert("Passwords do not match");
-                        return;
-                    }
-                    try {
-                        await createUserWithEmailAndPassword(
-                            auth,
-                            email.value,
-                            password.value
-                        );
-                    } catch (error) {
-                        alert(error);
-                    }
-                }}
-            >
+            <Form className="login-form" method="POST">
                 <h1>Sign Up</h1>
                 <p>
                     Already have an account? <Link to="/signin">Login</Link>
@@ -42,7 +18,7 @@ export default function Signup() {
                 <label htmlFor="confirm">Confirm Passowrd</label>
                 <input type="password" name="confirm" id="confirm" required />
                 <button type="submit">Sign Up</button>
-            </form>
+            </Form>
         </div>
     );
 }
